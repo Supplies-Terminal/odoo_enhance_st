bf2282806f094f0ab481ca4a0ea3ae5cc9cbd8c5
alvin.z <alvin.z.ca@gmail.com>
2022-09-06 14:56:46 -0400

    added search to res_partner

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,6 +6,7 @@ from odoo import _, api, fields, models, tools
 class Partner(models.Model):
     _inherit = "res.partner"
 
+    @api.model
     def name_get(self):
         res = []
         for rec in self:
@@ -14,3 +15,11 @@ class Partner(models.Model):
             else:
                 res.append((rec.id, "%s" % rec.name))
         return res
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+
+        if not (name == '' and operator == 'ilike'):
+            args += ['|', (self._rec_name, operator, name), ('ref', operator, name)]
+        return self._search(args, limit=limit, access_rights_uid=name_get_uid)
