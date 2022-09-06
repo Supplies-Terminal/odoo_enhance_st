d34f6074810b127baea1b8e6a7d915a04f92e25d
alvin.z <alvin.z.ca@gmail.com>
2022-09-06 15:34:49 -0400

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -10,16 +10,8 @@ class Partner(models.Model):
     def name_get(self):
         res = []
         for rec in self:
-            if (not rec.ref):
+            if rec.ref:
                 res.append((rec.id, "%s (%s)" % (rec.name, rec.ref)))
             else:
                 res.append((rec.id, "%s" % rec.name))
         return res
-
-    @api.model
-    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
-        args = args or []
-
-        if not (name == '' and operator == 'ilike'):
-            args += ['|', (self._rec_name, operator, name), ('ref', operator, name)]
-        return self._search(args, limit=limit, access_rights_uid=name_get_uid)
