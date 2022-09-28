cb3484fb5bd6375078443d5570d7ff3b701207e0
alvin.z <alvin.z.ca@gmail.com>
2022-09-28 13:20:52 -0600

    debug

--- models/st_purchasecard.py
+++ models/st_purchasecard.py
@@ -4,7 +4,7 @@ class StPurchasecard(models.Model):
     _name = "st.purchasecard"
     _description = "Purchase Card"
 
-    website_id = fields.Many2one('website', ondelete='cascade')
-    member_id = fields.Many2one('res.partner', string='Member')
+    website_id = fields.Many2one('website', ondelete='cascade', required=True)
+    member_id = fields.Many2one('res.partner', string='Member', required=True)
     uuid = fields.Char()
     data = fields.Text()
