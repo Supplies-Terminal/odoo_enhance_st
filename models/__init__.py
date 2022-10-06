6cf8c4fba8a2bb6528fee7b8235816831bf19085
alvin.z <alvin.z.ca@gmail.com>
2022-10-06 13:20:36 -0600

    Revert "扩展product.product"
    This reverts commit 1dd8411290388f2927ec56dec9929e3a19f9b453.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import product_product
 from . import res_partner
 from . import st_preference
 from . import st_purchasecard
