2374888883c11cbdc70000b93a1309c380970b2c
alvin.z <alvin.z.ca@gmail.com>
2022-10-06 13:41:31 -0600

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -8,13 +8,13 @@ class Partner(models.Model):
 
     website_ids = fields.Many2many('website', string='App Websites')
 
-    @api.model
-    def name_get(self):
-        res = []
-        res_partner_search_mode = self.env.context.get('res_partner_search_mode')
-        for rec in self:
-            if (rec.ref and res_partner_search_mode in ['customer', 'supplier']):
-                res.append((rec.id, "%s (%s)" % (rec.name, rec.ref)))
-            else:
-                res.append((rec.id, "%s" % rec.name))
-        return res
+    # @api.model
+    # def name_get(self):
+    #     res = []
+    #     res_partner_search_mode = self.env.context.get('res_partner_search_mode')
+    #     for rec in self:
+    #         if (rec.ref and res_partner_search_mode in ['customer', 'supplier']):
+    #             res.append((rec.id, "%s (%s)" % (rec.name, rec.ref)))
+    #         else:
+    #             res.append((rec.id, "%s" % rec.name))
+    #     return res
