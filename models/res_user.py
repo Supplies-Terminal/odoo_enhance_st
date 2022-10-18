cb87a39fb96979119688c1b2f0d1da82ecf4736d
alvin.z <alvin.z.ca@gmail.com>
2022-10-18 13:27:57 -0600

    Update res_user.py

--- models/res_user.py
+++ models/res_user.py
@@ -13,16 +13,16 @@ class ResUsers(models.Model):
 
 
     # Validate the user
-    @classmethod
-    def authenticate(cls, db, login, password, user_agent_env):
-        uid = super(ResUsers, cls).authenticate(db, login, password, user_agent_env)
-        if uid :
-            with cls.pool.cursor() as cr:
-                env = api.Environment(cr, uid, {})
-                user = env.user
-                if not user.approved_user and user._get_signup_invitation_scope() == 'b2c' and not user._is_superuser() and not user._is_admin():
-                    raise AccessDenied("Not Approved User")
-        return uid
+    # @classmethod
+    # def authenticate(cls, db, login, password, user_agent_env):
+    #     uid = super(ResUsers, cls).authenticate(db, login, password, user_agent_env)
+    #     if uid :
+    #         with cls.pool.cursor() as cr:
+    #             env = api.Environment(cr, uid, {})
+    #             user = env.user
+    #             if not user.approved_user and user._get_signup_invitation_scope() == 'b2c' and not user._is_superuser() and not user._is_admin():
+    #                 raise AccessDenied("Not Approved User")
+    #     return uid
 
     # Approve user
     def action_approve_user(self):
