bd1488bd9ecc42b69cfa966c7c1ee1dd0c4d5b9c
alvin.z <alvin.z.ca@gmail.com>
2022-10-19 13:53:22 -0600

    调试

--- models/product_product.py
+++ models/product_product.py
@@ -137,21 +137,6 @@ class ProductProduct(models.Model):
                 ])
                 domain = expression.AND([args, domain])
                 product_ids = list(self._search(domain, limit=limit, access_rights_uid=name_get_uid))
-            if not product_ids and operator in positive_operators:
-                ptrn = re.compile('(\[(.*?)\])')
-                res = ptrn.search(name)
-                if res:
-                    product_ids = list(self._search([('default_code', '=', res.group(2))] + args, limit=limit, access_rights_uid=name_get_uid))
-            # still no results, partner in context: search on supplier info as last hope to find something
-            if not product_ids and self._context.get('partner_id'):
-                # 搜索当前语言
-                suppliers_ids = self.env['product.supplierinfo']._search([
-                    ('name', '=', self._context.get('partner_id')),
-                    '|',
-                    ('product_code', operator, name),
-                    ('product_name', operator, name)], access_rights_uid=name_get_uid)
-                if suppliers_ids:
-                    product_ids = self._search([('product_tmpl_id.seller_ids', 'in', suppliers_ids)], limit=limit, access_rights_uid=name_get_uid)
 
                 # 扩展其它语言的内容，扩充到结果中
                 _logger.info("扩展其它语言的内容，扩充到结果中")
@@ -160,17 +145,36 @@ class ProductProduct(models.Model):
                 langs = [x[0] for x in installed_langs if x[0] != context_lang]
                 _logger.info(langs)
                 for lang in langs:
-                    suppliers_ids = self.with_context(lang=lang).env['product.supplierinfo']._search([
-                        ('name', '=', self._context.get('partner_id')),
-                        '|',
-                        ('product_code', operator, name),
-                        ('product_name', operator, name)], access_rights_uid=name_get_uid)
-                    if suppliers_ids:
-                        res_lang = self.with_context(lang=lang)._search([('product_tmpl_id.seller_ids', 'in', suppliers_ids)], limit=limit, access_rights_uid=name_get_uid)
+                    res_lang = list(self.with_context(lang=lang)._search(domain, limit=limit, access_rights_uid=name_get_uid))
+                    new_res = list(filter(lambda x: x not in product_ids, res_lang))
+                    _logger.info(new_res)
+                    product_ids.extend(new_res)
+            if not product_ids and operator in positive_operators:
+                ptrn = re.compile('(\[(.*?)\])')
+                res = ptrn.search(name)
+                if res:
+                    product_ids = list(self._search([('default_code', '=', res.group(2))] + args, limit=limit, access_rights_uid=name_get_uid))
 
-                        new_res = list(filter(lambda x: x not in product_ids, res_lang))
-                        _logger.info(new_res)
-                        product_ids.extend(new_res)
+                # 扩展其它语言的内容，扩充到结果中
+                _logger.info("扩展其它语言的内容，扩充到结果中")
+                context_lang = self._context.get("lang")
+                installed_langs = self.env["res.lang"].get_installed()
+                langs = [x[0] for x in installed_langs if x[0] != context_lang]
+                _logger.info(langs)
+                for lang in langs:
+                    res_lang = list(self.with_context(lang=lang)._search([('default_code', '=', res.group(2))] + args, limit=limit, access_rights_uid=name_get_uid))
+                    new_res = list(filter(lambda x: x not in product_ids, res_lang))
+                    _logger.info(new_res)
+                    product_ids.extend(new_res)
+            # still no results, partner in context: search on supplier info as last hope to find something
+            if not product_ids and self._context.get('partner_id'):
+                suppliers_ids = self.env['product.supplierinfo']._search([
+                    ('name', '=', self._context.get('partner_id')),
+                    '|',
+                    ('product_code', operator, name),
+                    ('product_name', operator, name)], access_rights_uid=name_get_uid)
+                if suppliers_ids:
+                    product_ids = self._search([('product_tmpl_id.seller_ids', 'in', suppliers_ids)], limit=limit, access_rights_uid=name_get_uid)
         else:
             product_ids = self._search(args, limit=limit, access_rights_uid=name_get_uid)
         return product_ids
