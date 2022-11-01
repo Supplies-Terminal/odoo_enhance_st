f907a114cf652eee6f808ff32d8ab77e397d2bf8
alvin.z <alvin.z.ca@gmail.com>
2022-11-01 14:30:13 -0600

    Update st_purchasecard.py

--- models/st_purchasecard.py
+++ models/st_purchasecard.py
@@ -5,10 +5,10 @@ class StPurchasecard(models.Model):
     _name = "st.purchasecard"
     _description = "Purchase Card"
 
+    def _generate_uuid(self):
+        return str(uuid.uuid4())
+
     website_id = fields.Many2one('website', ondelete='cascade', required=True)
     member_id = fields.Many2one('res.partner', string='Member', required=True)
     uuid = fields.Char(default=_generate_uuid)
     data = fields.Text()
-
-    def _generate_uuid(self):
-        return str(uuid.uuid4())
