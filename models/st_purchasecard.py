e833cd986f9ec1ea046c56982e1f939aced84b08
alvin.z <alvin.z.ca@gmail.com>
2022-11-01 14:12:17 -0600

    Update st_purchasecard.py

--- models/st_purchasecard.py
+++ models/st_purchasecard.py
@@ -7,7 +7,7 @@ class StPurchasecard(models.Model):
 
     website_id = fields.Many2one('website', ondelete='cascade', required=True)
     member_id = fields.Many2one('res.partner', string='Member', required=True)
-    uuid = fields.Char(compute='_compute_uuid', store=True, readonly=True)
+    uuid = fields.Char(compute='_compute_uuid', store=True)
     data = fields.Text()
 
     def _compute_uuid(self):
