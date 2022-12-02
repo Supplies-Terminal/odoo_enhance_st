e96e98266995c48e2b3214a53169ac2e12c38d67
alvin.z <alvin.z.ca@gmail.com>
2022-12-02 16:20:13 -0700

    调整计价字段

--- models/product_template.py
+++ models/product_template.py
@@ -7,13 +7,19 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    secondary_uom = fields.Many2one('uom.uom', 'Secondary UOM')
-    secondary_uom_name = fields.Char(
-        "Secondary UOM",
-        related='sh_secondary_uom.name'
-    )
-    secondary_unit_enabled = fields.Boolean("Enable Secondary Unit?")
-    secondary_uom_rate = fields.Float('Rate to Primary Unit')
+    secondary_uom_enabled = fields.Boolean("Enable Counting Unit?")
+    secondary_uom_id = fields.Many2one('uom.uom', 'Counting Unit')
+    secondary_uom_rate = fields.Float('Counting Unit to Primary Unit', default=1)
+
+    secondary_uom_desc = fields.Char(string='Counting Unit', compute='_compute_secondary_uom_desc', store=False)
+
+    @api.depends('secondary_uom_enabled', 'uom_id', 'secondary_uom_id', 'secondary_uom_rate')
+    def _compute_secondary_uom_desc(self):
+        for rec in self:
+            if rec.secondary_uom_enabled:
+                rec.secondary_uom_desc = "%s (%s%s)" % (rec.secondary_uom.name, rec.secondary_uom_rate, rec.uom_id.name)
+            else:
+                rec.secondary_uom_desc = ""
 
     # def action_open_sh_quants(self):
     #     if self:
