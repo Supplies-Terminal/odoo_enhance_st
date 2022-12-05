64a1b6baed56008889d1632024199f87332b31eb
alvin.z <alvin.z.ca@gmail.com>
2022-12-05 11:16:29 -0700

    扩展字段

--- models/product_template.py
+++ models/product_template.py
@@ -10,7 +10,10 @@ class ProductTemplate(models.Model):
     secondary_uom_enabled = fields.Boolean("Enable Counting Unit?")
     secondary_uom_id = fields.Many2one('uom.uom', 'Counting Unit')
     secondary_uom_rate = fields.Float('Counting Unit Rate', default=1)
-
+    secondary_uom_name = fields.Char(
+        "Counting Unit",
+        related='secondary_uom_id.name'
+    )
     secondary_uom_desc = fields.Char(string='Counting Unit', compute='_compute_secondary_uom_desc', store=False)
 
     @api.depends('secondary_uom_enabled', 'uom_id', 'secondary_uom_id', 'secondary_uom_rate')
