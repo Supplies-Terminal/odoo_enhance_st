c987c255b49a63fa42f687f6acede99fbde58620
alvin.z <alvin.z.ca@gmail.com>
2023-06-19 12:47:47 -0600

    debug

--- wizard/stock_orderpoint_replace.py
+++ wizard/stock_orderpoint_replace.py
@@ -2,7 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-from odoo.tools.date_utils import add
 
 import logging
 _logger = logging.getLogger(__name__)
