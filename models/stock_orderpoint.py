1e5ef87f5edf4df72663e6c6757f6c073e0ad50f
alvin.z <alvin.z.ca@gmail.com>
2023-07-16 19:24:48 -0400

    replenishment自动设置最后一个供应商

--- models/stock_orderpoint.py
+++ models/stock_orderpoint.py
@@ -19,3 +19,46 @@ class StockWarehouseOrderpoint(models.Model):
         })
         action['res_id'] = res.id
         return action
+
+    @api.depends('product_id')
+    def _auto_set_vendor(self):
+        for rec in self:
+            rec.latest_vendor = ''
+            _logger.info("------------_auto_set_vendor------------")
+            current_date = datetime.now().date()
+            previous_date = current_date - timedelta(days=1)
+                        
+            PurchaseOrderLineSudo = self.env['purchase.order.line'].sudo();
+            pol = PurchaseOrderLineSudo.search([('product_id', '=', rec.product_id.id), ('order_id.state', 'in', ['purchase', 'done']), ('create_date', '>=', previous_date)], limit=1, order='create_date desc')
+            if pol:
+                _logger.info(pol.order_id.partner_id.name)
+                suppliers = rec.product_id.suppliers.filtered(lambda s: s.partner_id.id == pol.order_id.partner_id.id)
+                _logger.info(suppliers)
+                if suppliers:
+                    rec.supplier_id = suppliers[0].id
+
+    @api.model
+    def create(self, vals):
+        _logger.info('*******---*stock.warehouse.orderpoint*---********')
+        rec = super(StockWarehouseOrderpoint, self).create(vals)
+        _logger.info(rec)
+        _logger.info(rec.product_id.variant_seller_ids)
+        _logger.info("------------_auto_set_vendor------------")
+        current_date = datetime.now().date()
+        previous_date = current_date - timedelta(days=1)
+        _logger.info("-----1------")
+        PurchaseOrderLineSudo = self.env['purchase.order.line'].sudo();
+        pol = PurchaseOrderLineSudo.search([('product_id', '=', rec.product_id.id), ('order_id.state', 'in', ['purchase', 'done']), ('create_date', '>=', previous_date)], limit=1, order='create_date desc')
+        _logger.info("-----2------")
+        if pol:
+            _logger.info("-----3------")
+            _logger.info(pol)
+            suppliers = rec.product_id.variant_seller_ids.filtered(lambda s: s.name.id == pol.order_id.partner_id.id)
+            _logger.info("-----4------")
+            _logger.info(suppliers)
+            if suppliers:
+                rec.write({
+                    'supplier_id': suppliers[0].id
+                })
+           
+        return rec
