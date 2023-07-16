5b04b25aaba886bf5d52b9e6b9c2e8cad3ccf1ea
alvin.z <alvin.z.ca@gmail.com>
2023-07-16 16:49:22 -0400

    Revert "debug"
    This reverts commit 6c8c3346c9f9f6ab113a5d51f54759a2d17dcce7.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-        # 'wizard/stock_orderpoint_replace_views.xml'
 {
     'name': 'ST Enhanced Toolkit',
     'category': 'Website/Website',
@@ -27,6 +26,7 @@
         'views/product_template_views.xml',
         'wizard/wishlist_wizard_views.xml',
         'wizard/wishlist_wizard_product_views.xml',
+        'wizard/stock_orderpoint_replace_views.xml'
     'demo': [
     ],
     'installable': True,
