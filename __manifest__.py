2152b09df022f5b1c4dc294174843e2e2b7f45db
alvin.z <alvin.z.ca@gmail.com>
2023-07-16 16:52:31 -0400

    完全清理搬货补货

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-        # 'wizard/wishlist_wizard_product_views.xml',
-        # 'wizard/stock_orderpoint_replace_views.xml'
 {
     'name': 'ST Enhanced Toolkit',
     'category': 'Website/Website',
@@ -26,7 +24,7 @@
         'views/template.xml',
         'views/product_template_views.xml',
         'wizard/wishlist_wizard_views.xml',
-        'views/stock_orderpoint_views.xml',
+        'wizard/wishlist_wizard_product_views.xml',
     'demo': [
     ],
     'installable': True,
--- models/__init__.py
+++ models/__init__.py
@@ -12,4 +12,3 @@ from . import sale_order_line
 from . import purchase_order_line
 from . import stock_move
 from . import account_invoice
-from . import stock_orderpoint
