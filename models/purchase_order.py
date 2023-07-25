f4d0eea288acba4548e8541d866e6a5885094ff7
alvin.z <alvin.z.ca@gmail.com>
2023-07-25 13:06:29 -0600

    调试PO的当天总数计算

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -3,6 +3,8 @@
 from datetime import datetime, time
 from dateutil.relativedelta import relativedelta
 import json
+import logging
+_logger = logging.getLogger(__name__)
 
 from odoo import api, fields, models, _
 
@@ -10,12 +12,24 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     today_po_count = fields.Integer(
-        string="Today's Purchase Orders",
-        compute='_compute_today_purchase_order_count'
+        string="Purchase Orders with Same Confirmation Date",
+        compute='_compute_today_purchase_order_count',
+        store=False
     )
     
     def _compute_today_purchase_order_count(self):
-        today = datetime.now().date()
-        domain = [('create_date', '>=', today), ('state', 'in', ['purchase', 'done'])]
-        today_purchase_order_count = self.search_count(domain)
-        self.today_po_count = today_purchase_order_count
+        for order in self:
+            order.today_po_count = 0
+            
+            _logger.info('-_compute_today_purchase_order_count -')
+            _logger.info(order.date_approve)
+            if order.date_approve:
+                date_approve = order.date_approve.date()
+    
+                start_of_day = datetime.combine(date_approve, time.min)
+                end_of_day = datetime.combine(date_approve, time.max)
+                domain = [('date_approve', '>=', start_of_day), ('date_approve', '<=', end_of_day), ('state', 'in', ['purchase', 'done'])]
+    
+                confirmation_date_purchase_order_count = self.search_count(domain)
+                _logger.info(confirmation_date_purchase_order_count)
+                order.today_po_count = confirmation_date_purchase_order_count
