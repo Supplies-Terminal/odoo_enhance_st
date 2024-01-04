59bbe8e340934543a5e2c5e31faa3424c3c3f5b8
alvin.z <alvin.z.ca@gmail.com>
2024-01-04 14:10:50 -0500

    Update product_configurator_widget.js

--- static/src/js/product_configurator_widget.js
+++ static/src/js/product_configurator_widget.js
@@ -7,10 +7,10 @@ odoo.define("odoo_enhance_st.product_configurator", function (require) {
       _onProductChange: function (event) {
         // 调用父类的_onProductChange方法
         this._super.apply(this, arguments);
-        // 截取 * 之后的内容
+        // 截取 ｜ 之后的内容
         if (this.$(".product_id").val()) {
           var productName = this.$(".product_id").select2("data")[0].text;
-          var trimmedName = productName.split("*")[0].trim();
+          var trimmedName = productName.split("｜")[0].trim();
           this.$(".product_id").select2("data")[0].text = trimmedName;
         }
       },
