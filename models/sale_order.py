09ac4b10ea2a28cd290c070b7f278e557f2d647e
alvin.z <alvin.z.ca@gmail.com>
2024-01-15 15:45:31 -0700

    inter-company pricing

--- models/sale_order.py
+++ models/sale_order.py
@@ -161,3 +161,53 @@ class SaleOrder(models.Model):
                 })
 
         return result
+
+
+    def intercompany_pricing_button_action(self):
+        _logger.info('- intercompany_pricing_button_action -')
+
+        self.ensure_one()  # 确保这个方法只在单一记录集上调用
+        
+        if self.state != 'sale':
+            raise UserError('Order must be confirmed to perform this action.')
+
+        # 确定销售订单应该属于哪个公司
+        internal_company = self.env['res.company'].search([('partner_id', '=', self.partner_id.id)], limit=1)
+        if not internal_company:
+            raise UserError('Customer is not an internal company.')
+        
+        # # 创建销售订单
+        # sale_order_vals = {
+        #     'partner_id': self.partner_id.id,
+        #     'company_id': internal_company.id,
+        #     # 以下是其他可能需要设置的字段
+        #     'origin': self.name,  # 可能需要将原始采购订单设置为来源
+        # }
+        
+        # # 添加订单行
+        # order_lines = []
+        # for line in self.order_line:
+        #     order_lines.append((0, 0, {
+        #         'product_id': line.product_id.id,
+        #         'product_uom_qty': line.product_qty,
+        #         'price_unit': line.price_unit,
+        #         # 您可能还需要设置其他必要的字段
+        #     }))
+        
+        # sale_order_vals['order_line'] = order_lines
+        # sale_order = self.env['sale.order'].create(sale_order_vals)
+        
+        # 根据需要进行其他操作，比如确认销售订单
+        # sale_order.action_confirm()
+
+        message = _('Sale order created successfully! Order #%s', sale_order.name)
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': _('Success'),
+                'message': message,
+                'sticky': False,  # 消息不会一直停留在屏幕上
+            }
+        }
+        return True
