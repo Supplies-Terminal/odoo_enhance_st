4c61ffdb7d1b08297f2455b69fabd3083c2eed68
alvin.z <alvin.z.ca@gmail.com>
2024-03-01 15:59:38 -0700

    Update mrp_bom_line.py

--- models/mrp_bom_line.py
+++ models/mrp_bom_line.py
@@ -3,7 +3,7 @@
 
 from odoo import models, fields
 
-class CustomMrpBomLine(models.Model):
-    _inherit = 'mrp.bom.line'  # 或者是 'mrp.bom' 如果是 BOM本身的数量
+class MrpBomLine(models.Model):
+    _inherit = 'mrp.bom.line'
 
     product_qty = fields.Float(digits=(16, 3))  # 设置为16位整数和3位小数
