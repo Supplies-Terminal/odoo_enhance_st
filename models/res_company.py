c9a481b76941ed28460278fe5eb6c896c7345dce
Alvin Z <alvin.z.ca@gmail.com>
2024-05-27 16:08:38 -0600

    company加上特殊location参数

--- models/res_company.py
+++ models/res_company.py
@@ -6,3 +6,6 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     private_product_only = fields.Boolean(string='Private Products Only', default=False)
+
+    ricai_location_id = fields.Many2one('stock.location', string='日采发货区', domain=[('usage', '=', 'internal')])
+    mrp_location_id = fields.Many2one('stock.location', string='生产物料区', domain=[('usage', '=', 'internal')])
