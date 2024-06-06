97b3437fb0e0e83194abb220bf105a61b9213378
Alvin Z <alvin.z.ca@gmail.com>
2024-06-06 14:40:33 -0600

    收货分流处理

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,7 +1,8 @@
 # -*- coding: UTF-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api
+from odoo import _, models, fields, api
+from odoo.exceptions import UserError, ValidationError
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -80,42 +81,77 @@ class StockPicking(models.Model):
     #         self.env['stock.move'].create(move_vals)
 
     @api.model
-    def _create_internal_transfer(self, source_location, dest_location, product_id, quantity, origin):
-        transfer = self.env['stock.picking'].create({
-            'location_id': source_location.id,
-            'location_dest_id': dest_location.id,
-            'picking_type_id': self.env.ref('stock.picking_type_internal').id,
-            'origin': origin,
-            'move_lines': [(0, 0, {
-                'product_id': product_id.id,
+    def _create_internal_transfer(self, source_location, dest_location, product_quantities, origin, picking_type_id):
+        if not source_location or not dest_location:
+            raise UserError(f"Missing location...")
+            
+        move_lines = []
+        for product, quantity in product_quantities.items():
+            move_lines.append((0, 0, {
+                'product_id': product.id,
                 'product_uom_qty': quantity,
-                'product_uom': product_id.uom_id.id,
+                'product_uom': product.uom_id.id,
                 'location_id': source_location.id,
                 'location_dest_id': dest_location.id,
-                'name': product_id.display_name,
-            })],
-        })
+                'name': product.display_name,
+            }))
+        _logger.info(move_lines)
+        vals = {
+            'location_id': source_location.id,
+            'location_dest_id': dest_location.id,
+            'picking_type_id': picking_type_id,
+            'origin': origin,
+            'move_lines': move_lines,
+        }
+        _logger.info(vals)
+        transfer = self.env['stock.picking'].create(vals)
         transfer.action_confirm()
         transfer.action_assign()
         return transfer
 
-    @api.multi
-    # 收货自动分区存放
     def button_validate(self):
         res = super(StockPicking, self).button_validate()
-        if self.picking_type_id.code == 'incoming' and self.company_id.mrp_location_id and self.company_id.ricai_location_id:
-            _logger.info('button_validate: 收货自动分区存放')
+        if self.picking_type_id.code == 'incoming':
+            _logger.info('----------button_validate-----------')
+            so_product_quantities = {}
+            mo_product_quantities = {}
+            
             for move in self.move_lines:
                 purchase_line = move.purchase_line_id
                 if purchase_line:
-                    if purchase_line.so_ids:
-                        dest_location = self.env.user.company_id.ricai_location_id
-                        origin = _('Purchase Order: %s (SO Transfer)') % (purchase_line.order_id.name)
-                        for so in purchase_line.so_ids:
-                            self._create_internal_transfer(self.location_dest_id, dest_location, move.product_id, so.quantity, origin)
-                    if purchase_line.mo_ids:
-                        dest_location = self.env.user.company_id.mrp_location_id
-                        origin = _('Purchase Order: %s (MO Transfer)') % (purchase_line.order_id.name)
-                        for mo in purchase_line.mo_ids:
-                            self._create_internal_transfer(self.location_dest_id, dest_location, move.product_id, mo.quantity, origin)
+                    for so in purchase_line.so_ids:
+                        product = move.product_id
+                        if product in so_product_quantities:
+                            so_product_quantities[product] += so.quantity
+                        else:
+                            so_product_quantities[product] = so.quantity
+                    for mo in purchase_line.mo_ids:
+                        product = move.product_id
+                        if product in mo_product_quantities:
+                            mo_product_quantities[product] += mo.quantity
+                        else:
+                            mo_product_quantities[product] = mo.quantity
+
+            _logger.info(so_product_quantities)
+            _logger.info(mo_product_quantities)
+            picking_type_internal = self.env['stock.picking.type'].search([
+                ('code', '=', 'internal'),
+                ('warehouse_id.company_id', '=', self.company_id.id),
+                ('name', 'ilike', 'Internal')
+            ], limit=1)
+
+            if not picking_type_internal:
+                raise UserError(f"No internal transfer opertation type found...")
+            
+            if so_product_quantities:
+                _logger.info(self.company_id.name)
+                dest_location = self.company_id.ricai_location_id
+                origin = _('Purchase Order: %s (SO Transfer)') % (self.origin)
+                self._create_internal_transfer(self.location_dest_id, dest_location, so_product_quantities, origin, picking_type_internal.id)
+
+            if mo_product_quantities:
+                dest_location = self.company_id.mrp_location_id
+                origin = _('Purchase Order: %s (MO Transfer)') % (self.origin)
+                self._create_internal_transfer(self.location_dest_id, dest_location, mo_product_quantities, origin, picking_type_internal.id)
+
         return res
