03f4627418db19ff47d0b90dff8e29234ff4ac24
Alvin Z <alvin.z.ca@gmail.com>
2024-06-07 17:16:46 -0600

    改为在po confirm的时候生成收货单

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -113,6 +113,95 @@ class PurchaseOrder(models.Model):
     def print_allocation_report(self):
         return self.env.ref('odoo_enhance_st.action_report_purchase_order_allocation').report_action(self)
 
+
+    def button_confirm(self):
+        # 如果是replenishment采购单
+        if self.picking_type_id.code == 'incoming' and ('replenishment' in self.origin.lower() or '补货' in self.origin.lower()):
+            if not self.company_id.ricai_location_id or not self.company_id.mrp_location_id:
+                raise UserError(f"Missing location...")
+            _logger.info('拆分收货单')
+            self._seperate_receiving_pickings()
+            
+        res = super(PurchaseOrder, self).button_confirm()
+        return res
+
+    def _seperate_receiving_pickings(self):
+        for order in self:
+            so_moves = []
+            mo_moves = []
+            stock_moves = []
+
+            for line in order.order_line:
+                product = line.product_id
+                remaining_qty = line.product_qty
+
+                # 分配给销售订单
+                so_total = 0;
+                for so in line.so_ids:
+                    if remaining_qty > 0:
+                        line_qty = min(remaining_qty, so.quantity)
+                        remaining_qty -= line_qty
+                        so_total += line_qty
+                        
+
+                # 分配给制造订单
+                mo_total = 0;
+                for mo in line.mo_ids:
+                    if remaining_qty > 0:
+                        line_qty = min(remaining_qty, mo.quantity)
+                        remaining_qty -= line_qty
+                        mo_total += line_qty
+
+                if so_total > 0:
+                    so_moves.append((product, so_total))
+
+                if mo_total > 0:
+                    mo_moves.append((product, mo_total))
+
+                if remaining_qty > 0:
+                    stock_moves.append((product, remaining_qty, ))
+            _logger.info('so_moves')
+            _logger.info(so_moves)
+            _logger.info('mo_moves')
+            _logger.info(mo_moves)
+            _logger.info('stock_moves')
+            _logger.info(stock_moves)
+            if so_moves:
+                self._create_seperated_picking(order, so_moves, _('Ricai Transfer'), order.company_id.ricai_location_id.id)
+            if mo_moves:
+                self._create_seperated_picking(order, mo_moves, _('MRP Transfer'), order.company_id.mrp_location_id.id)
+            if stock_moves:
+                self._create_seperated_picking(order, stock_moves, _('Stock Transfer'), order.picking_type_id.default_location_dest_id.id)
+
+    def _create_seperated_picking(self, order, moves, origin, dest_location_id):
+        picking_type = self.env['stock.picking.type'].search([
+            ('code', '=', 'incoming'),
+            ('warehouse_id.company_id', '=', order.company_id.id)
+        ], limit=1)
+
+        move_lines = [(0, 0, {
+            'product_id': product.id,
+            'product_uom_qty': qty,
+            'product_uom': product.uom_id.id,
+            'location_id': order.partner_id.property_stock_supplier.id,
+            'location_dest_id': dest_location_id,
+            'name': product.display_name,
+        }) for product, qty in moves]
+
+        picking = self.env['stock.picking'].create({
+            'location_id': order.partner_id.property_stock_supplier.id,
+            'location_dest_id': dest_location_id,
+            'picking_type_id': picking_type.id,
+            'origin': origin,
+            'move_lines': move_lines,
+            'partner_id': order.partner_id,
+        })
+        _logger.info(origin)
+        _logger.info(picking)
+        picking.action_confirm()
+        picking.action_assign()
+        return picking
+        
 class ReportPurchaseOrderAllocation(models.AbstractModel):
     _name = 'report.odoo_enhance_st.report_purchase_order_allocation'
 
