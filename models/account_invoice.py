93348c47776a0f3c93a75bd9629e05ce366fc4e7
Alvin Z <alvin.z.ca@gmail.com>
2025-01-23 15:34:13 -0700

    更换日结单为以商品为对象（跳过直接指定account的问题）

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -103,20 +103,19 @@ class AccountInvoice(models.Model):
                 settlement_invoice = self.env['account.move'].create({
                     'company_id': operating_company.id,
                     'move_type': 'out_invoice',
+                    'invoice_origin': 'Daily Settlement',
                     'invoice_date': settlement_date,
                     'partner_id': sales_company.partner_id.id,
-                    'journal_id': operating_journal.id,
                     'line_ids': [(0, 0, {
-                        'name': 'Daily Settlement',
+                        'product_id': 14245,
                         'price_unit': total_amount,
                         'quantity': 1,
-                        'account_id': operating_account.id,
                     })],
                 })
             else:
                 settlement_invoice.write({
                     'line_ids': [(0, 0, {
-                        'name': 'Daily Settlement',
+                        'product_id': 14245,
                         'price_unit': total_amount,
                         'quantity': 1,
                         'account_id': operating_account.id,
@@ -128,25 +127,24 @@ class AccountInvoice(models.Model):
                 settlement_bill = self.env['account.move'].create({
                     'company_id': sales_company.id,
                     'move_type': 'in_bill',
+                    'invoice_origin': 'Daily Settlement',
                     'invoice_date': settlement_date,
                     'partner_id': operating_company.partner_id.id,
-                    'journal_id': sales_journal.id,
                     'line_ids': [(0, 0, {
-                        'name': 'Daily Settlement',
+                        'product_id': 14245,
                         'price_unit': total_amount,
                         'quantity': 1,
-                        'account_id': sales_account.id,
                     })],
                 })
             else:
                 settlement_bill.write({
                     'line_ids': [(0, 0, {
-                        'name': 'Daily Settlement',
+                        'product_id': 14245,
                         'price_unit': total_amount,
                         'quantity': 1,
-                        'account_id': sales_account.id,
                     })],
                 })        
+
     def _set_next_sequence(self):
         if self.move_type == 'out_invoice':
             if not self.company_id.private_contact_only and not self.company_id.private_product_only and not self.company_id.is_virtual:
